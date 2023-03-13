2435b4603b51e671c3c4a5ba816d5e1b5dd0844e
Benedetti Davide <davidebenedetti1@gmail.com>
2023-03-13 22:47:56 +0100

    creata la grafica della tabella

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#define RIGHE 6
+#define COLONNE 7
 
 //crea tabella di 7 di base e 6 di altezza
 void printTable(){
-    
+    for (int i = 0; i < RIGHE; ++i) {
+        for (int j = 0; j < COLONNE; ++j) {
+            printf("|   ");
+            if(j == 6 ) printf("|");
+        }
+        printf("\n");
+    }
 }
 
-
 int main() {
-    printf("Hello, World!\n");
+    printTable();
     return 0;
 }
